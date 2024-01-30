d2df24cf234a02c250dcb60c532fd1b8656263cf
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-30 17:44:44 +0530

    Update : changed not found page styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,8 @@
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="bg-gray-200 w-full min-h-[calc(100vh-80px)] flex items-center justify-center">
+      <h5>Page Not Found</h5>
+    </div>
+  );
 };
 export default NotFound;
