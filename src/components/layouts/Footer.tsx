f84bdc92ce16215f18bf29b3a9748fd507bf80bb
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-30 16:34:08 +0530

    Update : Made footer component responsive

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,8 +1,8 @@
 import { whiteLogo } from "../../assets/images";
 const Footer = () => {
   return (
-    <footer className="w-full h-[80px] bg-secondary py-[24px]">
-      <div className="section-container">
+    <footer className="w-full h-[80px] bg-secondary py-auto">
+      <div className="section-container h-full 2xl:px-0 px-[18px]">
         <img src={whiteLogo} alt="Logo" className="brand-logo" />
       </div>
     </footer>
