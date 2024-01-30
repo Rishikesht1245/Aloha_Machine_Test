a72f674919a341d39d3f07bb1860786241235e94
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-30 17:46:22 +0530

    Update : resolved bug in category card

--- src/components/subComponents/CategoryCard.tsx
+++ src/components/subComponents/CategoryCard.tsx
@@ -5,7 +5,7 @@ const CategoryCard = ({ category }: { category: Categories }) => {
   return (
     <div className="flex justify-between items-center w-full md:max-w-[544px] h-[68px] bg-white p-[24px] rounded-[8px] hover:shadow-md">
       <p>{category?.name}</p>
-      <Link to={`/highlights/${category?.link}`}>
+      <Link to={category?.link}>
         <i className="fa-solid fa-arrow-right text-[16px] text-primary hover:translate-x-1 transition-all duration-300"></i>
       </Link>
     </div>
