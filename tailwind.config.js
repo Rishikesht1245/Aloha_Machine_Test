6df21241008e0318f94470a69b1663969551bcdc
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-30 10:51:58 +0530

    Installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {
+      colors: {
+        primary: "#008080",
+      },
+    },
+  },
+  plugins: [],
+};
