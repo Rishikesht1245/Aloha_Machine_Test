b8d29f306a67086faca857de0f91e8de1d408ab6
Rishikesh T <rishikeshtgit@gmail.com>
2024-01-30 15:49:33 +0530

    Update : Added image exports

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,3 +1,6 @@
 import brandLogo from "../assets/Aloha.png";
+import whiteLogo from "../assets/whiteLogo.png";
+import heroBg from "../assets/heroBg.png";
+import guide from "../assets/guide.png";
 
-export { brandLogo };
+export { brandLogo, whiteLogo, heroBg, guide };
